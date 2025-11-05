9fa9f5b855a27539def42d305e34ef294cacfd7b
Azenga Kevin <azenga.kevin7@gmail.com>
2025-11-05 17:07:56 +0300

    - User update and store requests - Soft deletes for a users resource - Users controller and routes setup

--- routes/api/users.php
+++ routes/api/users.php
@@ -4,4 +4,9 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class, 'index'])->name('index');
+Route::post('/', [UserController::class, 'store'])->name('store');
 Route::get('/me', [UserController::class, 'me'])->name('me')->middleware('auth:sanctum');
+Route::get('/{user}', [UserController::class, 'show'])->name('show');
+Route::put('/{user}', [UserController::class, 'update'])->name('update');
+Route::patch('/{user}', [UserController::class, 'update'])->name('update');
+Route::delete('/{user}', [UserController::class, 'destroy'])->name('destroy');
